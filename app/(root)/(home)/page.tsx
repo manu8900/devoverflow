e4fd81942864678115a86eaeb8497a90c716d48c
Manu George <gmanu19@gmail.com>
2023-10-11 21:04:20 +0530

    [Enhancement]:Added rout groups and  routes  in app folder.

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,12 @@
+// import Image from 'next/image'
+
+export default function Home() {
+    return (
+      <>
+      <h1 className="h1-bold">Next.js 13 we are coming!</h1>
+      <h2 className="h2-bold">Next.js 13 we are coming!</h2>
+      <h3 className="h3-bold">Next.js 13 we are coming!</h3>
+      </>
+    )
+  }
+  
