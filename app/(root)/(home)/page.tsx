da325932122118d994b6868af68cfbf1417abea5
Manu George <gmanu19@gmail.com>
2023-10-15 13:54:12 +0530

    [Enhancement]:Configured clerk auth with the application.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,9 @@
-// import Image from 'next/image'
-
+import { UserButton } from "@clerk/nextjs";
+ 
 export default function Home() {
-    return (
-      <>
-      <h1 className="h1-bold">Next.js 13 we are coming!</h1>
-      <h2 className="h2-bold">Next.js 13 we are coming!</h2>
-      <h3 className="h3-bold">Next.js 13 we are coming!</h3>
-      </>
-    )
-  }
-  
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/"/>
+    </div>
+  )
+}
